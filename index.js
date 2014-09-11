8ff24da3bbf3c82132e4eb6cbcd754668ba45620
Will Anderson <will@itsananderson.com>
2014-09-11 01:01:28 -0700

    Add initialization and opening of a repository

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var edge = require('edge');
 
-var myFunction = edge.func(function() {/*
+var init = edge.func(function Init() {/*
     #r "netlib/LibGit2Sharp.0.19.0.0/lib/net40/LibGit2Sharp.dll"
 
+    using System;
+    using System.Threading.Tasks;
     using LibGit2Sharp;
 
-    async (path) => {
-        return Repository.Init(path.ToString());
+    public class Startup {
+        public async Task<object> Invoke(object input) {
+            var path = input.ToString();
+            var initPath = Repository.Init(path);
+            Console.WriteLine(initPath);
+            using (var repo = new Repository(initPath)) {
+                return repo.Info.Path;
+            }
+        }
     }
 */});
 
-myFunction('./test', function(err, result) {
+init('./test', function(err, result) {
     if (err) throw err;
     console.log(result);
 });
