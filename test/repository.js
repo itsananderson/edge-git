183f60094fc55fef2d5fc018f092569479e4ff5e
Will Anderson <will@itsananderson.com>
2014-09-12 23:34:49 -0700

    Convert tests to use new repo implementation

--- test/repository.js
+++ test/repository.js
@@ -1,14 +1,14 @@
 var assert = require('assert'),
     path = require('path'),
     rimraf = require('rimraf'),
-    repository = require('../lib/repository');
+    repository = require('../lib/repo')(null, true);
 
 describe('repository', function() {
     it('can be initialized', function(done) {
         var repoDir = path.join(path.dirname(__dirname), 'repos', 'test1');
-        repository.init(repoDir, function(err, repo) {
+        repository.Init(repoDir, function(err, repoPath) {
             if (err) throw err;
-            assert.equal(repo.path, path.join(repoDir, '.git/'));
+            assert.equal(repoPath, path.join(repoDir, '.git/'));
             done();
         });
     });
@@ -16,9 +16,10 @@ describe('repository', function() {
     it('can clone', function(done) {
         var repoDir = path.join(path.dirname(__dirname), 'repos', 'test2');
         rimraf.sync(repoDir);
-        repository.clone('https://github.com/itsananderson/node-web-server-cli.git', repoDir, function(err, repo) {
+        repository.Clone({url:'https://github.com/itsananderson/node-web-server-cli.git', path:repoDir}, function(err, repoPath) {
             if (err) throw err;
-            assert.equal(repo.path, path.join(repoDir, '.git/'));
+
+            assert.equal(repoPath, path.join(repoDir, '.git/'));
             done();
         });
     });
