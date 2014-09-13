dcb2a50235adfa30824b426ec5968ec572f51846
Will Anderson <will@itsananderson.com>
2014-09-12 23:29:55 -0700

    Add more Repository properties

--- lib/repo.cs
+++ lib/repo.cs
@@ -12,6 +12,20 @@ public class Startup {
             Constructor = (Func<object, Task<object>>)(async (path) => {
                 var repo = new Repository(path.ToString());
                 return new {
+                    Head = (Func<object, Task<object>>)(async (i) => { return repo.Head; }),
+                    Config = (Func<object, Task<object>>)(async (i) => { return repo.Config; }),
+                    Index = (Func<object, Task<object>>)(async (i) => { return repo.Index; }),
+                    Ignore = (Func<object, Task<object>>)(async (i) => { return repo.Ignore; }),
+                    Network = (Func<object, Task<object>>)(async (i) => { return repo.Network; }),
+                    ObjectDatabase = (Func<object, Task<object>>)(async (i) => { return repo.ObjectDatabase; }),
+                    Refs = (Func<object, Task<object>>)(async (i) => { return repo.Refs; }),
+                    Commits = (Func<object, Task<object>>)(async (i) => { return repo.Commits; }),
+                    Tags = (Func<object, Task<object>>)(async (i) => { return repo.Tags; }),
+                    Stashes = (Func<object, Task<object>>)(async (i) => { return repo.Stashes; }),
+                    Info = (Func<object, Task<object>>)(async (i) => { return repo.Info; }),
+                    Diff = (Func<object, Task<object>>)(async (i) => { return repo.Diff; }),
+                    Notes = (Func<object, Task<object>>)(async (i) => { return repo.Notes; }),
+                    Submodules = (Func<object, Task<object>>)(async (i) => { return repo.Submodules; }),
                     Branches = (Func<object,Task<object>>)(async (i) => {
                         return repo.Branches.Select(b => new {
                             IsRemote = b.IsRemote,
@@ -26,7 +40,9 @@ public class Startup {
                             Commits = (Func<object,Task<object>>)(async (j) => { return b.Commits; }),
                             Name = b.Name
                         }).ToArray();
-                    })
+                    }),
+                    Dispose = (Func<object, Task<object>>)(async (i) => { repo.Dispose(); return null; }),
+                    Lookup = (Func<object, Task<object>>)(async (id) => { return repo.Lookup(id.ToString()); })
                 };
             }),
             Init = (Func<object, Task<object>>)(async (dynamic path) => {
@@ -35,6 +51,10 @@ public class Startup {
 
             Clone = (Func<object, Task<object>>)(async (dynamic input) => {
                 return Repository.Clone(input.url.ToString(), input.path.ToString());
+            }),
+
+            IsValid = (Func<object,Task<object>>)(async (path) => {
+                return Repository.IsValid(path.ToString());
             })
         };
     }
