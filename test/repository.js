6f54262f19bcc5e8bf1c97a878243bf5021a0b6b
Will Anderson <will@itsananderson.com>
2014-09-13 15:20:08 -0700

    Refactor repository lib to nicely wrap the edge api

--- test/repository.js
+++ test/repository.js
@@ -21,16 +21,31 @@ describe('repository', function() {
 
             assert.equal(repoPath, path.join(repoDir, '.git/'));
 
-            var repo = repository.Constructor(repoPath, true);
-            var branches = repo.Branches(null, true);
+            var repo = new repository(repoPath);
+            var branches = repo.BranchesSync();
             assert.equal(branches[0].Name(null, true), 'master');
 
             var tip = branches[0].Tip(null, true);
             var name = branches[0].Name(null, true);
-            var branchHead = repo.Lookup(name, true);
+            var branchHead = repo.LookupSync(name);
+
             assert.deepEqual(tip, branchHead);
 
             done();
         });
+    });
+
+    it('can list commits', function(done) {
+        this.timeout(10000);
+        var repoDir = path.join(path.dirname(__dirname), 'repos', 'test2', '.git');
+        var repo = new repository(repoDir, true);
+        var branches = repo.BranchesSync();
+        assert.equal(2, branches.length);
+
+        branches.forEach(function(branch) {
+            assert.equal(49, branch.Commits(null, true).length);
+        });
+
+        done();
     });
 });
