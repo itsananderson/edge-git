627e774d49991a54f608d6bea8af280d28cc5633
Will Anderson <will@itsananderson.com>
2014-09-17 10:51:02 -0700

    Update build process to download nuget and do a package restore

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
-var gulp = require('gulp'),
+var fs = require('fs'),
+    path = require('path'),
+    request = require('request'),
+    gulp = require('gulp'),
     mocha = require('gulp-mocha'),
-    msbuild = require('gulp-msbuild');
+    msbuild = require('gulp-msbuild'),
+    shelljs = require('shelljs');
 
-gulp.task('build', function() {
+var nugetPath = path.join(__dirname, 'netlib', 'nuget.exe');
+
+gulp.task('download-nuget', function(cb) {
+    fs.exists(nugetPath, function(exists) {
+        if (exists) {
+            return cb();
+        }
+        request('http://nuget.org/nuget.exe')
+            .on('end', cb)
+            .pipe(fs.createWriteStream(nugetPath));
+    });
+});
+
+gulp.task('package-restore', ['download-nuget'], function(cb) {
+    shelljs.exec(nugetPath + ' restore ./netlib/LibGit2SharpInvoke.sln', cb);
+});
+
+gulp.task('build', ['package-restore'], function() {
     return gulp.src('./netlib/LibGit2SharpInvoke.sln')
         .pipe(msbuild({
             errorOnFail: true,
