cd8bc4c812cbb7ad6c6c8bc2f9e2ac5a03c943cf
Will Anderson <will@itsananderson.com>
2014-09-16 22:01:49 -0700

    Properly rename LibGit2SharpInvoke.Console to LibGit2SharpInvoke.CommandLine

--- netlib/LibGit2SharpInvoke.CommandLine/Properties/AssemblyInfo.cs
+++ netlib/LibGit2SharpInvoke.CommandLine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibGit2SharpInvoke.Console")]
+[assembly: AssemblyTitle("LibGit2SharpInvoke.CommandLine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibGit2SharpInvoke.Console")]
+[assembly: AssemblyProduct("LibGit2SharpInvoke.CommandLine")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
