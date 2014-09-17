1092d56f939d213abfbc8d3d12fac617cf45447e
Will Anderson <will@itsananderson.com>
2014-09-16 21:13:50 -0700

    Add console application for testing out LibGit2Sharp functionality

---
+++ netlib/LibGit2SharpInvoke.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using System.IO;
+
+namespace LibGit2SharpInvoke.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            using (Repository r = new Repository("../../../../repos/test2/.git/"))
+            {
+                Console.WriteLine("test");
+            }
+        }
+    }
+}
